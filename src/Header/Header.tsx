be2e34478165f3ea2c963e85324b1606628b4d78
idaniths <idan.cheaproot@iths.se>
2024-10-25 17:37:16 +0200

    add header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Menu from '../Menu/Menu';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <div className="header-container">
+      <video autoPlay loop muted playsInline className="background-video">
+        <source src={`${process.env.PUBLIC_URL}/videos/patrikk.mov`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <h1 className="video-text">SWEDISH LOVE STORY</h1>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
