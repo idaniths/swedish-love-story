fcf1a417d525039ef808465890582396d54b3b9c
idaniths <idan.cheaproot@iths.se>
2024-10-27 14:20:10 +0100

    create app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header';
+import Video from './Screens/Video/Video'
+import Music from './Screens/Music/Music'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <div id="music"><Music /></div>      
+      <div id="video"><Video /></div>
+      <div id="contact">Contact Content</div>
+      <div id="social-media">Social Media Content</div>
     </div>
   );
 }
