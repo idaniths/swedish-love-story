00f1fb21433131d47d292beac5614b6ef09f2076
idaniths <idan.cheaproot@iths.se>
2024-10-27 14:21:12 +0100

    create video screen

--- src/Screens/Video/Video.tsx
+++ src/Screens/Video/Video.tsx
@@ -9,7 +9,12 @@ const VideoScreen: React.FC = () => {
           className="youtube-video"
           src="https://www.youtube.com/embed/SO44c9vycWY?si=VgBTfPMZn-l9yBVR"          
           title="YouTube Video"
-          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          className="youtube-video"
+          src="https://www.youtube.com/embed/Hbk37Pc0x3w?si=TzJCGbA5PWfQcqK3"          
+          title="YouTube Video"
           allowFullScreen
         ></iframe>
       </div>
