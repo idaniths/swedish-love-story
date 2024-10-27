ca92646a866feceeb550246c8109ed38e6a02752
idaniths <idan.cheaproot@iths.se>
2024-10-27 14:21:57 +0100

    create music screen

---
+++ src/Screens/Music/Music.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Music.css';
+
+const MusicScreen: React.FC = () => {
+  return (
+    <div className="music-screen">
+      
+    </div>
+  );
+};
+
+export default MusicScreen;
