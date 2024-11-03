a2908f3e0653ebeaceef5086f03ff41cb6eefab6
idaniths <idan.cheaproot@iths.se>
2024-11-03 15:59:52 +0100

    style for desktop

--- src/Pages/Music/Music.tsx
+++ src/Pages/Music/Music.tsx
@@ -8,20 +8,24 @@ const MusicScreen: React.FC = () => {
       <nav><Menu/></nav>
         <div className='album-container'>
         <div className='single-album'>
-          <a href="https://linktr.ee/swedishlovestory?lt_utm_source=lt_share_link#341094184"><img className="album-img"
+          <img className="album-img"
           src={`${process.env.PUBLIC_URL}/image/sls-milkys.png`} 
-          alt="Album Cover" /></a>
+          alt="Album Cover" />
+          <div className='text-content'>
           <h2>Milky's Ghost Ship Choir EP</h2>
           <p>Released: November 2023 </p>
           <a href="https://linktr.ee/swedishlovestory?lt_utm_source=lt_share_link#341094184">Listen</a>
           </div>
+          </div>
           <div className='single-album'>
-          <a href="https://linktr.ee/swedishlovestory?lt_utm_source=lt_share_link#233095368"><img className="album-img"
-        src={`${process.env.PUBLIC_URL}/image/wildlife.png`} 
-          alt="Album Cover" /></a>
+          <img className="album-img"
+          src={`${process.env.PUBLIC_URL}/image/wildlife.png`} 
+          alt="Album Cover" />
+          <div className='text-content'>
           <h2>Wildlife</h2>
           <p>Released: November 2021 </p>
           <a href="https://linktr.ee/swedishlovestory?lt_utm_source=lt_share_link#233095368">Listen</a>
+          </div>
           </div>
        
         </div>
